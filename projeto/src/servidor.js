cc14880a15f9a8c02b53a396e3f1adbac943d4ec
DouglasFortaleza <108711953+DouglasFortaleza@users.noreply.github.com>
2022-07-20 02:42:47 -0300

    Inserido put e delete
    Finalizado o CRUD com o delete adicionado

--- projeto/src/servidor.js
+++ projeto/src/servidor.js
@@ -24,6 +24,21 @@ app.post('/produtos', (req, res, next) => {
     res.send(produto) //JSON
 })
 
+// PUT serve para alterar um registro 
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) //JSON
+})
+
 app.listen(porta, () => {
     console.log(`Server listening on port ${porta}`)
 })
