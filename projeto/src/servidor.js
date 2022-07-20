e96d9ab70a1ca57814f2798ac6153e60d5de8f08
DouglasFortaleza <108711953+DouglasFortaleza@users.noreply.github.com>
2022-07-20 02:32:55 -0300

    Adicionado body parser
    Adicionado body-parser e suas dependencias.
    Também corrigido erro na busca do id

--- projeto/src/servidor.js
+++ projeto/src/servidor.js
@@ -2,19 +2,23 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true })) // Chama uma função middleware
+// Se o erro tiver o urlencoded, ele vai transformar para opbeto e aplicar o post
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos())
 })
 
-app.get('./produtos/:id', (req, res, next) => { // :id vai passar o valor do ID em numero
+app.get('/produtos/:id', (req, res, next) => { // :id vai passar o valor do ID em numero
     res.send(bancoDeDados.getProduto(req.params.id))
 }) 
 
 app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
     res.send(produto) //JSON
